034bae62e47128215b26ea8fa9002b49226b4786
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-05-21 12:24:48 +0100

    alx tasks

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """This script is the base model"""
-
 import uuid
 from datetime import datetime
 from models import storage
 
 
 class BaseModel:
-
     """Class from which all other classes will inherit"""
-
     def __init__(self, *args, **kwargs):
         """Initializes instance attributes
 
@@ -17,7 +14,6 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
@@ -36,20 +32,17 @@ class BaseModel:
 
     def __str__(self):
         """Returns official string representation"""
-
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
-
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
         """plus a key __class__ with the class name of the object."""
-
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
