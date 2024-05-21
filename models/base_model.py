54e06ffdec6e010435396e432b8ac464a8778093
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-05-21 12:43:04 +0100

    alx tasks

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""This script is the base model"""
 import uuid
 from datetime import datetime
-from models import storage
-
 
 class BaseModel:
     """Class from which all other classes will inherit"""
@@ -28,7 +25,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
@@ -38,7 +34,6 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
