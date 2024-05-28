756a9574934cd1fbd18120d4205621c0e3cd2097
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-05-28 15:33:33 +0100

    alx tasks

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-from models import storage
 
 class BaseModel:
     """Class from which all other classes will inherit"""
@@ -26,20 +25,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
         return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+            return f"[{class_name}] ({self.id}) {self.__dict__}"
 
-    def save(self):
+    def save(self)
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
+        """plus a key __class__ with the class name of the object."""
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
